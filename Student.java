59a4b07542158b3f5f6c78500ca54b75eb726098
sezgin <szgnmstfa@gmail.com>
2020-04-10 13:13:06 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -6,14 +6,65 @@ class Student {
 	int[] labPoints;
 
 	//TODO constructor
-
+	public Student(String index, String firstName, String lastName, int[] labPoints){
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
 	//TODO seters & getters
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int[] getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(int[] labPoints) {
+		this.labPoints = labPoints;
+	}
 
 	public double getAverage() {
 		//TODO
+		int avg=0;
+		for (int point: labPoints) {
+			avg += point;
+		}
+
+		avg /= labPoints.length;
+		return avg;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		int count=0;
+		for (int point: labPoints) {
+			if (point != null) count++;
+		}
+
+		if(count >= 8) return true;
+		return false;
 	}
 }
